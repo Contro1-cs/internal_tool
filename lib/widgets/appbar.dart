cc68650d42f5c5acd34573deb48462bd58d21bc2
Aaditya Jagdale <aadityajagdale.21@gmail.com>
2023-12-06 18:19:17 +0530

    app bar complete

--- lib/widgets/appbar.dart
+++ lib/widgets/appbar.dart
@@ -1,3 +1,5 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
@@ -46,25 +48,50 @@ class _HomeAppBarState extends State<HomeAppBar> {
     });
   }
 
-  userProfile(String name) {
-    return Container(
-      height: 60,
-      width: 60,
-      margin: const EdgeInsets.only(left: 10),
-      alignment: Alignment.center,
-      decoration: BoxDecoration(
-        color: yellow,
-        shape: BoxShape.circle,
-      ),
-      child: Text(
-        name[0].toUpperCase(),
-        textAlign: TextAlign.center,
-        style: GoogleFonts.inter(
-          fontSize: 35,
-          color: const Color(0xff8e7000),
+  userProfile(String imgUrl, String friendUid) {
+    return Column(
+      children: [
+        Container(
+          width: 60,
+          margin: const EdgeInsets.only(left: 10),
+          alignment: Alignment.center,
+          decoration: BoxDecoration(
+            color: yellow,
+            shape: BoxShape.circle,
+          ),
+          child: imgUrl.isEmpty
+              ? const SizedBox()
+              : Padding(
+                  padding: const EdgeInsets.all(1),
+                  child: ClipOval(
+                    child: CachedNetworkImage(
+                      imageUrl: imgUrl,
+                      placeholder: (context, url) => const Center(
+                        child: CircularProgressIndicator(
+                          strokeWidth: 0.5,
+                        ),
+                      ),
+                      errorWidget: (context, url, error) =>
+                          const Icon(Icons.error),
+                    ),
+                  ),
+                ),
         ),
-      ),
+      ],
     );
+  }
+
+  Future<String?> _getImageUrl(String folderName) async {
+    final storageRef =
+        FirebaseStorage.instance.ref().child(folderName).child("pfp.png");
+
+    try {
+      String downloadURL = await storageRef.getDownloadURL();
+      return downloadURL;
+    } catch (e) {
+      print('Error fetching image: $e');
+      return null;
+    }
   }
 
   @override
@@ -91,12 +118,19 @@ class _HomeAppBarState extends State<HomeAppBar> {
               shape: BoxShape.circle,
             ),
             child: _pfp.isEmpty
-                ? const CircularProgressIndicator()
+                ? const Center(child: CircularProgressIndicator())
                 : Padding(
                     padding: const EdgeInsets.all(1),
                     child: ClipOval(
-                      child: Image(
-                        image: NetworkImage(_pfp),
+                      child: CachedNetworkImage(
+                        imageUrl: _pfp,
+                        placeholder: (context, url) => const Center(
+                          child: CircularProgressIndicator(
+                            strokeWidth: 0.5,
+                          ),
+                        ),
+                        errorWidget: (context, url, error) =>
+                            const Icon(Icons.error),
                       ),
                     ),
                   ),
@@ -105,12 +139,22 @@ class _HomeAppBarState extends State<HomeAppBar> {
           //Friends
           Expanded(
             child: SizedBox(
-              height: 60,
               child: ListView.builder(
                 scrollDirection: Axis.horizontal,
                 itemCount: widget.friends.length,
                 itemBuilder: (context, index) {
-                  return userProfile(widget.friends[index]);
+                  String friendUid = widget.friends[index];
+
+                  return FutureBuilder(
+                    future: _getImageUrl(friendUid),
+                    builder: (BuildContext context, AsyncSnapshot snapshot) {
+                      String imageUrl = snapshot.data.toString();
+                      if (snapshot.connectionState == ConnectionState.done) {
+                        return userProfile(imageUrl, friendUid);
+                      }
+                      return const SizedBox();
+                    },
+                  );
                 },
               ),
             ),
